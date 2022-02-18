e637695b59ce8391a1bb5acc84e6b13a2d3ee0e2
manu189 <faciomanuel@gmail.com>
2022-02-18 18:58:04 -0300

    first commit

---
+++ scrapy_test2/spiders/quotes_js_spider.py
@@ -0,0 +1,26 @@
+import scrapy
+from scrapy_splash import SplashRequest
+
+
+class QuotesJsSpider(scrapy.Spider):
+    name = 'quotes_js_spider'
+    # allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/js/']
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield SplashRequest(url, self.parse)
+            # yield SplashRequest(url, self.parse, endpoint='render.html', args={'wait': 0.5})
+
+    def parse(self, response):
+        for quote in response.css("div.quote"):
+            yield {
+                'quote': quote.css("span.text::text").get(),
+                'author': quote.css("small.author::text").get(),
+                'tags': quote.css("div.tags a.tag::text").getall(),
+            }
+
+        next_page = response.css('li.next a::attr("href")').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield SplashRequest(next_page, callback=self.parse)
